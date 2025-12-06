07a47d116656a1db878227798e6e26ceec51e5a2
zcitium <ananya.khandkar@gmail.com>
2025-12-06 11:45:04 +0530

    Fix: Add WHITENOISE_STATIC_PREFIX for Vercel

--- stegano_project/settings.py
+++ stegano_project/settings.py
@@ -82,6 +82,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Vercel specific fixes
+WHITENOISE_STATIC_PREFIX = '/static/'
+if not DEBUG:
+     STATIC_URL = '/static/'
+
 # Logging Configuration
 LOGGING = {
     'version': 1,
