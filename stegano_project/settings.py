b4c83255b0eca03801b7f16712a384af00dfdd00
zcitium <ananya.khandkar@gmail.com>
2025-12-06 11:31:59 +0530

    Fix: Remove auth context processor from templates

--- stegano_project/settings.py
+++ stegano_project/settings.py
@@ -127,7 +127,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                # 'django.contrib.auth.context_processors.auth', # Removed for stateless serverless
                 'django.contrib.messages.context_processors.messages',
             ],
         },
