b2900a6c9bf12bf436d8dbfa064641bf3c66a593
zcitium <ananya.khandkar@gmail.com>
2025-12-06 11:29:38 +0530

    Fix: Add app alias for Vercel and ensure static dir exists

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
+mkdir -p staticfiles_build # Ensure directory exists
 python3.9 manage.py collectstatic --noinput
 # python3.9 manage.py migrate # Not needed for stateless app
