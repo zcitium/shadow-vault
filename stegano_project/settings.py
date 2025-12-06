b3fdb54662ec2e55a5a1c64c922e2e95e071c59d
zcitium <ananya.khandkar@gmail.com>
2025-12-06 18:01:41 +0530

    Fix: Escape JS messages

--- stegano_project/settings.py
+++ stegano_project/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware', # Removed for Vercel Native Static Files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
