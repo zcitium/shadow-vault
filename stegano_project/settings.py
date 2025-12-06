edf85a6628049cba96221ba85b511a01f6930a20
zcitium <ananya.khandkar@gmail.com>
2025-12-06 11:26:44 +0530

    Fix: Remove admin/auth apps for stateless deployment

--- stegano_project/settings.py
+++ stegano_project/settings.py
@@ -51,8 +51,8 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    # 'django.contrib.admin', # Removed for stateless serverless
+    # 'django.contrib.auth',  # Removed for stateless serverless
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware', # Removed
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
